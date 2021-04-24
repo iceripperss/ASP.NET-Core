20e2a6fff175a532f03913f415b222bace37e1fe
Peter Hryhoruk <lifeofzombie6@gmail.com>
2021-04-24 10:20:41 +0300

    commit1

---
+++ HelloApp/RoutingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace HelloApp
+{
+    public class RoutingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public RoutingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string path = context.Request.Path.Value.ToLower();
+            if (path == "/index")
+            {
+                await context.Response.WriteAsync("Home Page");
+            }
+            else if (path == "/about")
+            {
+                await context.Response.WriteAsync("About");
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+            }
+        }
+    }
+}
